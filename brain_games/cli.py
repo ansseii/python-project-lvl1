e90e4b50485e7eba73fec674e332e51f00d99365
Aleksandr Lukash <a.lukash.92@gmail.com>
2021-01-15 16:11:48 +0300

    fix linter issues

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,10 @@
+"""Command line interface."""
+
 import prompt
 
 
 def welcome_user():
+    """Greet player."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {0}!', name)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""Main."""
 
-from brain_games.cli import welcome_user
+from brain_games import cli
 
 
 def main():
-    welcome_user()
+    """Entry point."""
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
